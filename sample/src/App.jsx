79b7326335227fda9788f7c92ccf12fc7032e054
Nidharsan Velmurugan <nidharsanvelmurugan@jmangroup.com>
2026-03-02 16:08:22 +0530

    new chnage in app.jsxrkooooo

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -4,9 +4,8 @@ function App() {
   return (
     <div className="wonderful-page">
       <h1>Nidharsan</h1>
-      <h3>VIPER RKO</h3>
-      sadfasdfsadfasdfsdfsdfdsf
-      <h1>sadfsadfsagsfagdfgasfgsd</h1>
+      <h3>VIPER RKO00000000000000</h3>
+      
     
     </div>
   )
