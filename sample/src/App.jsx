054697ecb47187094f24439624d40d3d316edcd2
Nidharsan Velmurugan <nidharsanvelmurugan@jmangroup.com>
2026-03-02 15:59:03 +0530

    new chnage in app.jsx

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -3,8 +3,9 @@ import './App.css'
 function App() {
   return (
     <div className="wonderful-page">
-      <h1>Welcome to Nidharsan's Wonderful Page</h1>
-      <p>This page was crafted with care and 💫</p>
+      <h1>Nidharsan</h1>
+      <h3>VIPER RKO</h3>
+      
       <button className="action-button">Click me!</button>
     </div>
   )
