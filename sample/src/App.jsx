e4c3a827697e1801c571ee46bf8bc3ed2021fdcc
Nidharsan Velmurugan <nidharsanvelmurugan@jmangroup.com>
2026-03-02 11:58:25 +0530

    First push -- jenkins free style

---
+++ sample/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="wonderful-page">
+      <h1>Welcome to Nidharsan's Wonderful Page</h1>
+      <p>This page was crafted with care and 💫</p>
+      <button className="action-button">Click me!</button>
+    </div>
+  )
+}
+
+export default App
+
+
